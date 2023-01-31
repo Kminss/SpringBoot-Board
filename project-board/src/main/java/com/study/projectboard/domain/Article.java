b455320f8f11d040b3ba6273716f36481e8d211c
Kim MinJung <kminss3711@gmail.com>
2023-02-01 01:05:26 +0900

    #9 - 도메인Entity 및 Repository interface 추가
    * db에 접근하는 repository추가

--- project-board/src/main/java/com/study/projectboard/domain/Article.java
+++ project-board/src/main/java/com/study/projectboard/domain/Article.java
@@ -8,9 +8,12 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Getter
 @ToString
@@ -20,6 +23,7 @@ import java.util.Objects;
         @Index(columnList = "createdAt"),
         @Index(columnList = "createdBy")
 })
+@EntityListeners(AuditingEntityListener.class)
 @Entity
 public class Article {
     @Id
@@ -34,15 +38,16 @@ public class Article {
     private String content; // 본문
     @Setter
     private String hashtag; // 해시태그
-
+    @OrderBy("id")
+    @OneToMany(mappedBy = "article", cascade = {CascadeType.ALL})
+    private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
     //Meta Info
     @CreatedDate
     @Column(nullable = false)
-    private LocalDateTime createAt; // 생성일시
+    private LocalDateTime createdAt; // 생성일시
     @CreatedBy
     @Column(nullable = false, length = 100)
     private String createdBy; // 생성자
-
     @LastModifiedDate
     @Column(nullable = false)
     private LocalDateTime modifiedAt; // 수정일시
@@ -50,7 +55,7 @@ public class Article {
     @Column(nullable = false, length = 100)
     private String modifiedBy; // 수정자
 
-    protected Article (){
+    protected Article() {
 
     }
 
