dc55032361696644d0dfa25c1963ed105c7dd88d
Kim MinJung <kminss3711@gmail.com>
2023-02-08 23:53:11 +0900

    #15 - 게시판 뷰 skeleton 작성
    게시판 뷰페이지 기본 골격 작성

--- project-board/src/main/java/com/study/projectboard/controller/ArticleController.java
+++ project-board/src/main/java/com/study/projectboard/controller/ArticleController.java
@@ -1,7 +1,12 @@
 package com.study.projectboard.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
 /**
 /articles/search	GET	게시판 검색 전용 페이지
 /articles/search-hashtag	GET	게시판 해시태그 검색 전용 페이지
@@ -9,5 +14,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/articles")
 @Controller
 public class ArticleController {
-
+    @GetMapping
+    public String articles(ModelMap map) {
+        map.addAttribute("articles", List.of());
+        return "articles/index";
+    }
 }
